eae1bc0845882a564275c35f7af83acfbd61eb97
Your Name <you@example.com>
2024-08-22 16:28:30 +0530

    first commit

---
+++ auth/app/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path,include
+from .import views
+
+urlpatterns = [
+
+     path('register',views.register,name="register"),
+     path('add',views.add_details,name='add_details'),
+     path('',views.loginpage,name='loginpage'),
+     path('log',views.log,name='log'),
+     path('adminmod',views.adminmod,name='adminmod'),
+     path('usermod/<int:id>',views.usermod,name='usermod'),
+     path('lgout',views.lgout,name='lgout'),
+     path('list_user', views.list_user, name='list_user'),
+     path('delteacher/<int:id>', views.delteacher, name='delteacher'),
+
+]
